9cef9a27f100548221973a71d8f8889b0fbb3c63
Yashwantyadav1 <yy888275@gmail.com>
2025-04-03 23:47:14 +0530

    Hollow_Hourglass_Pattern.js

---
+++ 2nd day/12_palindrome_Triangular.js
@@ -0,0 +1,18 @@
+function palindromeTriangular(n){
+    let triangle="";
+    for(let i=0;i<n;i++){
+
+        for(let j=0;j<n-(i+1);j++){
+            triangle += "  ";
+        }
+        for(let k=i;k>=1;k--){
+            triangle += k + " ";
+        }
+        for(let l=2;l<=i;l++){
+            triangle +=l + " ";
+        }
+        triangle += "\n";
+    }
+    console.log(triangle);
+}
+palindromeTriangular(6);
