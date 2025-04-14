7c47a062bd785fc5933ad56092938d5bd760ec97
Yashwantyadav1 <yy888275@gmail.com>
2025-04-14 17:47:33 +0530

    Second_Highest_number.js

--- 3rd day/02_Second_Highest_number.js
+++ 3rd day/02_Second_Highest_number.js
@@ -1,3 +1,6 @@
-function SecondHeighestNumber(n){
-    
-}
+let numbers = [5, 9, 1, 3, 7, 9];
+let uniqueNumbers = [...new Set(numbers)];
+uniqueNumbers.sort((a, b) => b - a);
+let secondLargest = uniqueNumbers[1];
+
+console.log("Second largest number is:", secondLargest);
