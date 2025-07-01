97cc74471f9abf5212a4c6d5669c81386783050f
Yashwantyadav1 <yy888275@gmail.com>
2025-07-01 13:24:21 +0530

    3rd day js practice

---
+++ 3rd day/practice.js
@@ -0,0 +1,8 @@
+let yash=["home","about","contact"]
+document.writeln(`<nav> <ul> 
+    `)
+    for (let nav of yash){
+        document.writeln(`<li> ${nav}</li>`)
+
+    }
+    document.writeln(`</ul></nav>`)
